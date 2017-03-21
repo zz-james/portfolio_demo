2d171918d351c07f5f72f19f614a48866a302e29
James Cat <james@catplusplus.org.uk>
2017-03-21 15:30:55 +0000

    unit page

---
+++ unit.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Portfolio Demo : Unit View</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="build/main.css">
+</head>
+<body>
+
+  <div id="nav">
+    <ul class="side-nav">
+      <li>
+        <div class="userView">
+          <div class="background">
+            <img src="images/office.jpg">
+          </div>
+          <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+          <a href="#!name"><span class="white-text name">John Doe</span></a>
+          <a href="#!email"><span class="white-text email">jdandturk@coelrind.com</span></a>
+        </div>
+      </li>
+
+      <li><a href="index.php"><i class="material-icons">home</i>Home</a></li>
+      <li><a href="#!"><i class="material-icons">view_module</i>Qualification</a></li>
+      <li><a href="#!"><i class="material-icons">file_upload</i>Upload Evidence</a></li>
+      <li><a href="#!"><i class="material-icons">message</i>Messages</a></li>
+      <li><a href="#!"><i class="material-icons">perm_identity</i>Your Profile</a></li>
+    </ul>
+  </div>
+  <div id="content">
+    <br />
+    <div class="row"  style="margin-left: -70px">
+
+      <div class="col s9 offset-s3">
+        <nav>
+          <div class="nav-wrapper">
+            <div class="col s12">
+              <a href="#!" class="breadcrumb">Probation Case Administration</a>
+              <a href="#!" class="breadcrumb">Process information relating to individuals' offending behaviour</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+
+      <div class="col s9 offset-s3">
+        <div class="card-panel clearfix">
+          <p><strong>Level:</strong> 2</p>
+          <p><strong>Aims:</strong> This unit is about processing and recording information relating to individuals’ offending behaviour. When processing information, the learner needs to focus on accuracy and be able to communicate any difficulties should they arise. The information obtained needs to be processed and recorded for access by others.</p>
+          <div id="learning-outcomes">
+            <div class="col s12">
+              <table class="bordered">
+                <thead>
+                  <tr>
+                      <th data-field="id">Learning Outcome</th>
+                      <th data-field="name">Assesment Criteria</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td rowspan="3" width="50%">1. Understand the principles and organisational requirements that underpin processing information about the offending behaviour and circumstances of individuals <div class="green-text"><i class="material-icons">done_all</i>all complete</div></td>
+                    <td>1.1 Identify factors that influence offending behaviour of individuals.
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                  <tr>
+                    <td>1.2 Explain the importance of validity, reliability and sufficiency in relation to processing information about the offending behaviour 
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                  <tr>
+                    <td>1.3 Identify the legislation, organisational policies and procedures for processing, recording and communicating information about the behaviour and circumstances of individuals 
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="col s12">
+              <table class="bordered">
+                <thead>
+                  <tr>
+                      <th data-field="id">Learning Outcome</th>
+                      <th data-field="name">Assesment Criteria</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td rowspan="6" width="50%">2. Be able to process and communicate information about individuals and the offending behaviour and circumstances</td>
+                    <td>2.1 Identify the factors that need to be taken into account whilst processing information about individuals</td>
+                  </tr>
+                  <tr>
+                    <td>2.2 Confirm the relevant information about individuals with others</td>
+                  </tr>
+                  <tr>
+                    <td>2.3 Process information about individuals in line with organisational requirements
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                  <tr>
+                    <td>2.4 Provide information promptly to those who are entitled to receive it
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                  <tr>
+                    <td>2.5 Communicate information effectively with colleagues and others in line with organisational requirements</td>
+                  </tr>
+                  <tr>
+                    <td>2.6 Alert relevant others of any difficulties whilst processing information about individuals
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="col s12">
+              <table class="bordered">
+                <thead>
+                  <tr>
+                      <th data-field="id">Learning Outcome</th>
+                      <th data-field="name">Assesment Criteria</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td width="50%">3. Know how to maintain own competence in relation to processing information about the offending behaviour and circumstances of individuals
+                    <div class="green-text"><i class="material-icons">done_all</i>all complete</div></td>
+                    <td>3.1 Describe how to maintain own competence in this area of work
+                    <div class="green-text"><i class="material-icons">done</i>complete</div></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="build/bundle.js"></script>
+</body>
+</html>
