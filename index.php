d16d83991f5f1c9ac7c6f2ded1ff637bcc64fc17
James Cat <james@catplusplus.org.uk>
2017-03-21 13:01:00 +0000

    forgot to add stuff again

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Portfolio Demo</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="build/main.css">
+</head>
+<body>
+
+  <div id="nav">
+    <ul class="side-nav">
+      <li><div class="userView">
+        <div class="background">
+          <img src="images/office.jpg">
+        </div>
+        <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+        <a href="#!name"><span class="white-text name">John Doe</span></a>
+        <a href="#!email"><span class="white-text email">jdandturk@coelrind.com</span></a>
+      </div></li>
+      <li><a href="#!"><i class="material-icons">home</i>Home</a></li>
+      <li><a href="#!"><i class="material-icons">view_module</i>Qualification</a></li>
+      <li><a href="#!"><i class="material-icons">file_upload</i>Upload Evidence</a></li>
+      <li><a href="#!"><i class="material-icons">message</i>Messages</a></li>
+      <li><a href="#!"><i class="material-icons">perm_identity</i>Your Profile</a></li>
+      
+<!--       <li><div class="divider"></div></li>
+      <li><a class="subheader">Subheader</a></li>
+      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li> -->
+    </ul>
+  </nav>
+  <div id="content">
+  <!-- <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a> -->
+<br />
+  <div class="row"  style="margin-left: -70px">
+    <div class="col s9 offset-s3">
+      <div class="card-panel">
+
+        <div class="row">
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h5>Activity Log</h5>
+              <p class="light">
+                <ul id="activity-log">
+                  <li><span>Mar 2nd 2017</span>   <a href="#">Uploaded Evidence: Information Processing</a></li>
+                  <li><span>Jan 22nd 2017</span> <a href="#">Assessor sign off: Unit 3</a></li>
+                  <li><span>Jan 22nd 2017</span> <a href="#">Assessor sign off: Assessment Criteria 3.2.1</a></li>
+                  <li><span>Jul 9th 2016</span>   <a href="#">Assessor reassign Evidence</a></li>
+                  <li><span>Mar 30th 2016</span>  <a href="#">Uploaded Evidence: Call Handling</a></li>
+                </ul>
+              </p>
+            </div>
+          </div>
+
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h5>Progress</h5>
+              <div id="progress-container"></div>
+            </div>
+          </div>
+
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h5>Recent Messages</h5>
+
+                <ul class="collection">
+                  <li class="collection-item avatar">
+                    <img src="images/yuna.jpg" alt="" class="circle">
+                    <span class="title">Title</span>
+                    <p>First Line</p>
+                    <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                  </li>
+                  <li class="collection-item avatar">
+                    <i class="material-icons circle">folder</i>
+                    <span class="title">Title</span>
+                    <p>First Line</p>
+                    <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                  </li>
+                  <li class="collection-item avatar">
+                    <i class="material-icons circle green">insert_chart</i>
+                    <span class="title">Title</span>
+                    <p>First Line</p>
+                    <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                  </li>
+                </ul>
+
+
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+    <div class="row" id="units">
+      <div class="col s9 offset-s3">
+      <div class="card-panel clearfix">
+        <h5>Qualification: Probation Case Administration</h5><br/>
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title"><a href="#">Process information relating to individuals' offending behaviour</a></span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+       <!--        <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Respond to offender related enquiries</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+             <!--  <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Resolve customer service problems</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+        
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Maintain the security of information when communicating with people</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Receive, transmit, store and retrieve information </span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Evaluate and improve own performance in a business environment </span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Maintain and develop your own knowledge, skills and competence </span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Contribute to the quality of team working</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Work with other people in a business environment</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Equality and diversity</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+
+        <div class="col s3">
+          <div class="card small z-depth-3">
+            <div class="card-content">
+              <span class="card-title">Maintain personal security and safety, and be alert to the security of others</span>
+            </div>
+            <div class="card-action">
+              <span class="progress-bar-container"></span>
+              <!-- <a href="#">This is a link</a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="build/bundle.js"></script>
+</body>
+</html>
