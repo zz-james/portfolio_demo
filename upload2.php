d1ac9201a572ea78eeac205525e3fd46c18178ec
James Cat <james@catplusplus.org.uk>
2017-04-07 11:39:08 +0100

    getting to point where need react

--- upload2.php
+++ upload2.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html>
 
-  <head>
-    <title>Portfolio Demo</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="build/main.css">
-    <style>
-        input[type=text]{
-          width:200px;
-          padding:8px 10px;
-        }
+<head>
+  <title>Portfolio Demo</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="build/main.css">
+  <style>
+    input[type=text] {
+      width: 200px;
+      padding: 8px 10px;
+    }
+    
+    li em {
+      background: #ff6;
+      font-weight: bold;
+      font-style: normal;
+    }
+  </style>
+</head>
 
-        li em {
-          background:#ff6;
-          font-weight:bold;
-          font-style:normal;
-        }
-    </style>
-  </head>
-  <body>
+<body>
 
   <div id="nav">
     <ul class="side-nav">
@@ -48,8 +49,56 @@
     <!-- <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a> -->
     <br />
 
-    <div class="row" id="evidence" style="margin-left: -70px">
-      <div class="col s9 offset-s3">
+
+
+
+
+    <!--<div class="row" id="evidence" style="margin-left: -70px">
+
+      <div class="col s3 offset-s3">
+        <div class="evidence_picker" style="background: #fff; padding: 5px">
+
+          <div class="border">
+            <div class="arrow_box orange">
+              <span>UNIT</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;Communication in a business environment</span>
+          </div>
+          <div class="border">
+            <div class="arrow_box green">
+              <span>LEARNING OUTCOME</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;Understand the requirements of written and verbal business communication</span>
+          </div>
+          <div class="border">
+            <div class="arrow_box">
+              <span class="arrow_content">ASSESMENT CRITERIA</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;Explain why different communication methods are used in the business environment</span>
+          </div>
+          <div class="border">
+            <div class="arrow_box">
+              <span class="arrow_content">ASSESMENT CRITERIA</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;Describe the communication requirements of different audiences</span>
+          </div>
+          <div class="border">
+            <div class="arrow_box">
+              <span class="arrow_content">ASSESMENT CRITERIA</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;Explain the importance of using correct grammar, sentence structure, punctuation, spelling and conventions in business communications</span>
+          </div>
+          <div class="border">
+            <div class="arrow_box">
+              <span class="arrow_content">ASSESMENT CRITERIA</span>
+            </div>
+            <span>&nbsp;&nbsp;&nbsp;Explain the importance of using appropriate body language and tone of voice when communicating verbally</span>
+          </div>
+        </div>
+      <br />
+      </div>
+      <div class="col s3 offset-s3"></div>-->
+    <!--<div class="col s9 offset-s3">
         <input type="text" id="search-dinosaurs" placeholder="Search for Dinosaurs (start typing)" />
         
         <ul id="dino-list">
@@ -62,46 +111,197 @@
           <li>Iguanadon</li>
           <li>Velociraptor</li>
         </ul>
+      </div>-->
+    <!--<div class="col s3 offset-s3">
+
+        <div class="collection">
+          <a href="#!" class="collection-item">Communication in a business environment</a>
+          <!--<li class="collection-item">
+            <ul class="collection">
+              <li class="collection-item"><div class="arrow_box green"><span>LEARNING OUTCOME</span></div><br />
+                Understand the requirements of written and verbal business communication</li>
+              <li class="collection-item">
+                <ul class="collection">
+                  <li class="collection-item"><div class="arrow_box"><span class="arrow_content">ASSESMENT CRITERIA</span></div><br />
+                    Explain why different communication methods are used in the business environment</li>
+                  <li class="collection-item"><div class="arrow_box"><span class="arrow_content">ASSESMENT CRITERIA</span></div><br />
+                    Describe the communication requirements of different audiences</li>
+                  <li class="collection-item"><div class="arrow_box"><span class="arrow_content">ASSESMENT CRITERIA</span></div><br />
+                    Explain the importance of using correct grammar, sentence structure, punctuation, spelling and conventions in business communications</li>
+                  <li class="collection-item"><div class="arrow_box"><span class="arrow_content">ASSESMENT CRITERIA</span></div><br />
+                    Explain the importance of using appropriate body language and tone of voice when communicating verbally</li>
+                </ul>
+              </li>
+              <li class="collection-item">Be able to produce written business communications</li>
+              <li class="collection-item">Be able to communicate verbally in business environments</li>
+            </ul>
+          </li>-->
+    <!--<a href="#!" class="collection-item">Understand employer organisations</a>
+          <a href="#!" class="collection-item">Principles of providing administrative services</a>
+        </div>
+      
+      </div>
+    </div> -->
+
+    <div class="row">
+      <div class="card-panel col s6 offset-s3 white">
+
+        <div class="card">
+          <div class="arrow_box orange"><span>UNIT: </span><span>Communication in a business environment</div>
+          <div class="card-content">
+            <span><h5>Communication in a business environment</h5></span>
+            <span>17 Units</span> - <span>41 Assessment Criteria</span>
+          </div>
+          <div class="card-action">
+            <a class="btn blue darken-3"><i class="material-icons">expand_more</i></a>
+            <a class="btn-floating orange right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <div class="card z-depth-2">
+          <div class="arrow_box green"><span>LEARNING OUTCOME</span></div>
+          <div class="card-content">
+            <span><h5>Understand the requirements of written and verbal business communication</h5></span>
+          </div>
+          <div class="card-action">
+            <a class="btn blue darken-3"><i class="material-icons">expand_more</i></a>
+            <a class="btn-floating green right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <!-- assessment criteria -->
+        <div class="card z-depth-3">
+          <div class="arrow_box"><span>ASSESMENT CRITERIA</span></div>
+          <div class="card-content clearfix">
+            <span><h6>Identify the nature, purpose, audience and use of the information to be communicated</h6></span>
+            <a class="btn-floating blue right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <div class="card z-depth-3">
+          <div class="arrow_box"><span>ASSESMENT CRITERIA</span></div>
+          <div class="card-content clearfix">
+            <span><h6>Use communication channels that are appropriate to the information to be communicated and the audience</h6></span>
+            <a class="btn-floating blue right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <div class="card z-depth-3">
+          <div class="arrow_box"><span>ASSESMENT CRITERIA</span></div>
+          <div class="card-content clearfix">
+            <span><h6>Present information in the format that meets the brief</h6></span>
+            <a class="btn-floating blue right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <div class="card z-depth-3">
+          <div class="arrow_box"><span>ASSESMENT CRITERIA</span></div>
+          <div class="card-content clearfix">
+            <span><h6>Adhere to agreed business communication conventions and degree of formality of expression when producing documents</h6></span>
+            <a class="btn-floating blue right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <div class="card z-depth-3">
+          <div class="arrow_box"><span>ASSESMENT CRITERIA</span></div>
+          <div class="card-content clearfix">
+            <span><h6>Produce business communications that are clear, accurate and correct<h6></span>
+            <a class="btn-floating blue right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <div class="card z-depth-3">
+          <div class="arrow_box"><span>ASSESMENT CRITERIA</span></div>
+          <div class="card-content clearfix">
+            <span><h6>Meet agreed deadlines in communicating with others</h6></span>
+            <a class="btn-floating blue right"><i class="material-icons">add</i></a>
+          </div>
+        </div>
+
+        <!-- -->
+
+        <div class="card z-depth-2">
+          <div class="arrow_box green"><span>LEARNING OUTCOME</span></div>
+          <div class="card-content">
+            <span><h5>Be able to produce written business communications</h5></span>
+            <div class="card-action">
+              <a class="btn blue darken-3"><i class="material-icons">expand_more</i></a>
+              <a class="btn-floating green right"><i class="material-icons">add</i></a>
+            </div>
+          </div>
+        </div>
+    <!-- -->
+        <div class="card z-depth-2">
+          <div class="arrow_box green"><span>LEARNING OUTCOME</span></div>
+          <div class="card-content">
+            <span><h5>Be able to communicate verbally in business environments</h5></span>
+            <div class="card-action">
+              <a class="btn blue darken-3"><i class="material-icons">expand_more</i></a>
+              <a class="btn-floating green right sml"><i class="material-icons">add</i></a>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
+</div>
+  <!-- close row -->
+
+  <!--<div class="fixed-action-btn horizontal click-to-toggle">
+    <a class="btn-floating btn-large red">
+      <i class="material-icons">menu</i>
+    </a>
+    <ul>
+      <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+    </ul>
+  </div>-->
+
+
+  </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script>
-  $(document).ready(function () {
- 
-    /* initially hide product list items */
-    //$("#dino-list li").hide();
- 
-    /* highlight matches text */
-    var highlight = function (string) {
+    $(document).ready(function () {
+
+      /* initially hide product list items */
+      //$("#dino-list li").hide();
+
+      /* highlight matches text */
+      var highlight = function (string) {
         $("#dino-list li.match").each(function () {
-            var matchStart = $(this).text().toLowerCase().indexOf("" + string.toLowerCase() + "");
-            var matchEnd = matchStart + string.length - 1;
-            var beforeMatch = $(this).text().slice(0, matchStart);
-            var matchText = $(this).text().slice(matchStart, matchEnd + 1);
-            var afterMatch = $(this).text().slice(matchEnd + 1);
-            $(this).html(beforeMatch + "<em>" + matchText + "</em>" + afterMatch);
+          var matchStart = $(this).text().toLowerCase().indexOf("" + string.toLowerCase() + "");
+          var matchEnd = matchStart + string.length - 1;
+          var beforeMatch = $(this).text().slice(0, matchStart);
+          var matchText = $(this).text().slice(matchStart, matchEnd + 1);
+          var afterMatch = $(this).text().slice(matchEnd + 1);
+          $(this).html(beforeMatch + "<em>" + matchText + "</em>" + afterMatch);
         });
-    };
- 
- 
-    /* filter products */
-    $("#search-dinosaurs").on("keyup click input", function () {
+      };
+
+
+      /* filter products */
+      $("#search-dinosaurs").on("keyup click input", function () {
         if (this.value.length > 0) {
-            $("#dino-list li").removeClass("match").hide().filter(function () {
-                return $(this).text().toLowerCase().indexOf($("#search-dinosaurs").val().toLowerCase()) != -1;
-            }).addClass("match").show();
-            highlight(this.value);
-            //$("#dino-list").show();
+          $("#dino-list li").removeClass("match").hide().filter(function () {
+            return $(this).text().toLowerCase().indexOf($("#search-dinosaurs").val().toLowerCase()) != -1;
+          }).addClass("match").show();
+          highlight(this.value);
+          //$("#dino-list").show();
         }
         else {
-            $("#dino-list li").removeClass("match").show();
+          $("#dino-list li").removeClass("match").show();
         }
+      });
+
+
     });
- 
- 
-});
   </script>
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"></script>
+
   <!--<script src="build/bundle.js"></script>-->
 </body>
 
